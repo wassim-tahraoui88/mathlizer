b1ad48423f0667d025f74aed75b24d70a4f0af85
Wassim Tahraoui <wassim.tahraoui@horizon-tech.tn>
2025-04-18 17:14:51 +0100

    - Added/Extended features.

--- src/main/java/com/tahraoui/mathlizer/controller/data/request/DerivativeRequest.java
+++ src/main/java/com/tahraoui/mathlizer/controller/data/request/DerivativeRequest.java
@@ -1,4 +1,7 @@
 package com.tahraoui.mathlizer.controller.data.request;
 
-public record DerivativeRequest(String function, String variable) {
+public record DerivativeRequest(String function, String variable, Integer order) {
+	public DerivativeRequest {
+		if (order == null) order = 1;
+	}
 }
